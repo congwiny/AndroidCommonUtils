1483c92fa66151301e97a6c591f79ee33df423d8
zhenlvhe <zhenlvhe@kingdee.com>
2013-08-27 19:13:57 +0800

    添加了systemutils

---
+++ AndroidUtils/src/org/lib/androidutils/SystemUtils.java
@@ -0,0 +1,19 @@
+package org.lib.androidutils;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.view.View;
+
+public class SystemUtils {
+	/**
+	 * 返回状态栏（通知栏）的高度
+	 * @param context
+	 * @return
+	 */
+	public static int getStatusBarHeight(Context context) {
+		Rect frameRect = new Rect();
+		View view = new View(context);
+		view.getWindowVisibleDisplayFrame(frameRect);
+		return frameRect.top;
+	}
+}
