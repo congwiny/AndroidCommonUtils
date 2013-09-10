1943a253bd66354f261c445a43ebd385fa69e622
zhenlvhe <zhenlvhe@kingdee.com>
2013-09-10 14:34:22 +0800

    添加了getExactPixel,根据dpi计算像素值

--- AndroidUtils/src/org/lib/androidutils/SystemUtils.java
+++ AndroidUtils/src/org/lib/androidutils/SystemUtils.java
@@ -16,4 +16,15 @@ public class SystemUtils {
 		view.getWindowVisibleDisplayFrame(frameRect);
 		return frameRect.top;
 	}
+	
+	/**
+	 * 根据像素密度计算出不同分辨率机型应该显示的像素值
+	 * @param pixel
+	 * @return
+	 */
+	public static float getExactPixel(Context context, float pixel) {
+		int dip = context.getResources().getDisplayMetrics().densityDpi;
+		float factor = dip/160f;
+		return pixel*factor;
+	}
 }
