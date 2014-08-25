5ad6aef8fae6e443f2cff597a3e4707213b34cc2
zhenlv_he <zhenlv_he@kingdee.com>
2014-08-25 18:50:39 +0800

    添加配置文件到忽略列表中，添加ROOT获取代码

--- AndroidUtils/src/me/jeremyhe/lib/androidutils/SystemUtils.java
+++ AndroidUtils/src/me/jeremyhe/lib/androidutils/SystemUtils.java
@@ -1,5 +1,7 @@
 package me.jeremyhe.lib.androidutils;
 
+import java.io.DataOutputStream;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -57,5 +59,33 @@ public class SystemUtils {
 		}
 	}
 	
+	
+	/**
+	 * 应用程序运行命令获取 Root权限，设备必须已破解(获得ROOT权限)
+	 */
+	public static boolean upgradeRootPermission(String pkgCodePath) {
+	    Process process = null;
+	    DataOutputStream os = null;
+	    try {
+	        String cmd="chmod 777 " + pkgCodePath;
+	        process = Runtime.getRuntime().exec("su"); //切换到root帐号
+	        os = new DataOutputStream(process.getOutputStream());
+	        os.writeBytes(cmd + "\n");
+	        os.writeBytes("exit\n");
+	        os.flush();
+	        process.waitFor();
+	    } catch (Exception e) {
+	        return false;
+	    } finally {
+	        try {
+	            if (os != null) {
+	                os.close();
+	            }
+	            process.destroy();
+	        } catch (Exception e) {
+	        }
+	    }
+	    return true;
+	}
 
 }
