f809f48153c2014d6655282813daf377e4b6a3bf
zhenlv_he <zhenlv_he@kingdee.com>
2014-10-30 14:56:18 +0800

    添加图片转灰色函数

--- AndroidUtils/src/me/jeremyhe/lib/androidutils/BitmapUtils.java
+++ AndroidUtils/src/me/jeremyhe/lib/androidutils/BitmapUtils.java
@@ -3,6 +3,10 @@ package me.jeremyhe.lib.androidutils;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 
 public class BitmapUtils {
 	
@@ -33,4 +37,23 @@ public class BitmapUtils {
 	    options.inJustDecodeBounds = false;   
 	    return BitmapFactory.decodeResource(res, resId, options);   
 	} 
+	
+	/**
+	 * 根据源图片产生一张灰色的图片
+	 */
+	public static Bitmap toGrey(Bitmap bitmap) {
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
+
+		Bitmap faceIconGreyBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+		Canvas canvas = new Canvas(faceIconGreyBitmap);
+		Paint paint = new Paint();
+		ColorMatrix colorMatrix = new ColorMatrix();
+		colorMatrix.setSaturation(0);
+		ColorMatrixColorFilter colorMatrixFilter = new ColorMatrixColorFilter(colorMatrix);
+		paint.setColorFilter(colorMatrixFilter);
+		canvas.drawBitmap(bitmap, 0, 0, paint);
+		return faceIconGreyBitmap;
+	}
 }
